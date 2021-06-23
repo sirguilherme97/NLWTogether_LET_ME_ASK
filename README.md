9cdbbfe7e0b9f4ae842001332007c550323f2e72
SirGuilhermeBR <sir_guilherme@hotmail.com>
2021-06-23 04:06:14 -0300

    firebase, rooms, questions

---
+++ README.md
@@ -0,0 +1,4 @@
+# Criando Aplicação | NWL Together
+@[RocketSeat](https://github.com/Rocketseat)
+[Next Level Week Together](https://nextlevelweek.com).
+<img src="/src/assets/images/ReadMe.png"/>
---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Home } from '../src/pages/Home'
+import { NewRoom } from '../src/pages/NewRoom'
+import { AuthContextProvider } from './contexts/AuthContext'
+import { Room } from './pages/Room';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter >
+  );
+}
+export default App;
